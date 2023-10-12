da03529e15d03869666453a0bde1e5382771a891
Matheus Becker <beckerm@weg.net>
2023-10-11 21:47:14 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let contador = 1;
+const array = ["Linda", "Perfeita", "Carinhosa", "Estressada", "Minha melhor amiga", "Minha Jogadora", "Um pouco ciumenta", "Importante","Companheira", "Por fim, a minha princesa!"];
+
+const addTexto = (valor) => {
+
+
+    console.log(contador);
+    if (valor == contador) {
+        const quadrado = document.getElementById(contador);
+        quadrado.innerHTML = "<p class='texto'>"+ array[contador] + "</p>";
+        quadrado.classList.add("quadrado");
+        contador++;
+    }
+
+    if (contador == 9) {
+        setTimeout(() => {
+        window.location.replace("./pag2.html");
+    }, "60000");
+}
+
+}
